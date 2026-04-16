924ab9994ed8d1eeffdd5df3b7a44737426ce28c
kaido <leanhtuan02072006@gmail.com>
2026-04-17 01:29:32 +0700

    them routes kết nối

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Hiển thị form
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+
+// Xử lý logic
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
