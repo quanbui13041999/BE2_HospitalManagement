32bbc7fbebb7a982d26c56ab1d8d1546da780a2e
ken248 <anh2482006@gmail.com>
2026-04-22 16:24:20 +0700

    fix

--- database/migrations/2024_01_01_000001_create_roles_table.php
+++ database/migrations/2024_01_01_000001_create_roles_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('Roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('role_id');
             $table->string('role_name', 50)->unique();
         });
