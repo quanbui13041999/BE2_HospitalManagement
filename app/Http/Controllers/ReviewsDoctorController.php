fd6f0b0a1a1e29d4e19be9aa220cbc20d1735979
kaido <leanhtuan02072006@gmail.com>
2026-05-07 01:14:00 +0700

    luu danh gia

--- app/Http/Controllers/ReviewsDoctorController.php
+++ app/Http/Controllers/ReviewsDoctorController.php
@@ -6,6 +6,7 @@ use App\Services\ReviewsDoctorService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use function PHPUnit\Framework\throwException;
 
 class ReviewsDoctorController extends Controller
 {
@@ -32,6 +33,41 @@ class ReviewsDoctorController extends Controller
             'success' => $result['can'],
             'message' => $result['message'] ?? null
         ]);
+    }
+
+    /**
+     * luu danh gia
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) {
+        if (!$this->reviewsDoctorService->auth()) {
+            return response()->json(['success' => false, 'message' => 'Vui lòng đăng nhập để tiếp tục']);
+        }
+
+        try {
+            $vallidate = $request->validate(
+                $this->reviewsDoctorService->rules(),
+                $this->reviewsDoctorService->message()
+            );
+            $review = $this->reviewsDoctorService->store($vallidate, Auth::id());
+
+            return response()->json([
+                'sucess' => true,
+                'message' => 'Cảm ơn bạn đã đánh giá',
+                'review' => $review
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => collect($e->errors())->flatten()->first(),
+            ], 422);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Có lỗi xảy ra, vui lòng thử lại.',
+            ], 500);
+        }
     }
 }
 ?>
