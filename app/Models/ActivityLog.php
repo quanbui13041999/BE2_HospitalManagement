0a7c52bfebbfbe14f1ee99a24cf9768a7428e249
kaido <leanhtuan02072006@gmail.com>
2026-05-07 02:16:44 +0700

    fix model

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
-    //
+   protected $table      = 'activitylogs';
+    protected $primaryKey = 'log_id';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = null;
+
+    protected $fillable = ['user_id','action','ip_address'];
+    protected $casts    = ['created_at' => 'datetime'];
+
+    public function user() { return $this->belongsTo(User::class, 'user_id', 'user_id'); }
 }
