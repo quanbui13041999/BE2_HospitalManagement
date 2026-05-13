9b2379f3583a52b9a80a9ae0b4b854480ccf7686
kaido <leanhtuan02072006@gmail.com>
2026-05-13 16:13:45 +0700

    liên kết tới trang index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -676,6 +676,7 @@
                 @auth
                 <li><a href="{{ route('profile.show') }}">Hồ sơ</a></li>
                 <li><a href="{{ route('appointments.index') }}">Lịch hẹn</a></li>
+                <li><a href="{{ route('doctor.schedule') }}">Lịch làm việc</a></li>
                 @endauth
                 <li><a href="{{ route('doctors.index') }}">Bác sĩ</a></li>
                 <li><a href="{{ route('user.services.index') }}">Khoa phòng</a></li>
