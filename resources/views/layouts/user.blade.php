3881cadb94570b2e1ef871bcffa2a19f5d214450
ken248 <anh2482006@gmail.com>
2026-05-14 15:59:11 +0700

    sửa ui

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -17,6 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 
+    
     <style>
         body {
             font-family: 'Inter', sans-serif;
